34c968c53c3a27104c39ad8d6f642e00964483ac
Tim Kuijpers <timkuijpers2002@outlook.com>
2023-11-07 13:39:36 +0100

    removed too complex code

--- ReportQueryAPI/Data/ReportQueryDbContext.cs
+++ ReportQueryAPI/Data/ReportQueryDbContext.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ReportQueryAPI.DAL.Context
+namespace ReportQueryAPI.Data
 {
     public class ReportQueryDbContext : DbContext
     {
---
+++ ReportQueryAPI/Services/GPReportQueryService.cs
@@ -0,0 +1,58 @@
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
+using ReportQueryAPI;
+using ReportQueryAPI.Data;
+
+namespace ReportQueryAPI.Services
+{
+    public class GPReportQueryService : GPReportQueryProto.GPReportQueryProtoBase
+    {
+        private readonly ReportQueryDbContext _dbContext;
+        public GPReportQueryService(ReportQueryDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public override async Task<ReadGPReportResponse> ReadGPReport(ReadGPReportRequest request, ServerCallContext context)
+        {
+            if (request.Id == string.Empty)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "Resource index must be greater than 0"));
+            }
+            var gpreport = await _dbContext.GPReports.FirstOrDefaultAsync(gpr => gpr.Id.ToString() == request.Id);
+
+            if (gpreport != null)
+            {
+                return await Task.FromResult(new ReadGPReportResponse
+                {
+                    Id = gpreport.Id.ToString(),
+                    PatientId = gpreport.PatientId.ToString(),
+                    EmployeeId = gpreport.EmployeeId.ToString(),
+                    InitialCreation = gpreport.InitialCreation.ToUniversalTime().ToTimestamp()
+                });
+            }
+
+            throw new RpcException(new Status(StatusCode.NotFound, $"No gpreport with id {request.Id}"));
+        }
+
+        public override async Task<GetAllGPReportResponse> GetAllGPReport(GetAllGPReportRequest request, ServerCallContext context)
+        {
+            var response = new GetAllGPReportResponse();
+            var gpreports = await _dbContext.GPReports.ToListAsync();
+
+            foreach (var gpreport in gpreports)
+            {
+                response.GpReport.Add(new ReadGPReportResponse
+                {
+                    Id = gpreport.Id.ToString(),
+                    PatientId = gpreport.PatientId.ToString(),
+                    EmployeeId = gpreport.EmployeeId.ToString(),
+                    InitialCreation = gpreport.InitialCreation.ToUniversalTime().ToTimestamp()
+                }); ;
+            }
+
+            return await Task.FromResult(response);
+        }
+    }
+}
