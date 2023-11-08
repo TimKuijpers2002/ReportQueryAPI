7e2994c391bc864c288b1cf928e0fb0bccdc63cf
TimKuijpers2002 <timkuijpers2002@outlook.com>
2023-11-08 09:28:55 +0100

    added dbcon string

--- ReportQueryAPI/Program.cs
+++ ReportQueryAPI/Program.cs
@@ -6,19 +6,13 @@ using ReportQueryAPI.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Additional configuration is required to successfully run gRPC on macOS.
-// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-
-// Add services to the container.
-builder.Services.AddGrpc().AddJsonTranscoding();
-
 ConfigurationManager configuration = builder.Configuration;
-builder.Services.AddDbContext<ReportQueryDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
-    b => b.MigrationsAssembly("FeedMessages.API").EnableRetryOnFailure()));
+builder.Services.AddDbContext<ReportQueryDbContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddGrpc().AddJsonTranscoding();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.MapGrpcService<GPReportQueryService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
