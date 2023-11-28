d60fd723020566d93ba6aeb440b475941cdd6e5e
Tim Kuijpers <timkuijpers2002@outlook.com>
2023-11-28 21:41:01 +0100

    added reportcommand endpoints + docker-compose elements

--- ReportQueryAPI/Program.cs
+++ ReportQueryAPI/Program.cs
@@ -14,6 +14,6 @@ builder.Services.AddGrpc().AddJsonTranscoding();
 var app = builder.Build();
 
 app.MapGrpcService<GPReportQueryService>();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.MapGet("/", () => "health check");
 
 app.Run();
