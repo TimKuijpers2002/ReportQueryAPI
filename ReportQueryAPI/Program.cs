05eec5c20cc45e7beea565ed746c49a044b4d0b6
Tim Kuijpers <timkuijpers2002@outlook.com>
2023-12-18 16:37:08 +0100

    added all k8s files & new azure sql connection string for report query api

--- ReportQueryAPI/Program.cs
+++ ReportQueryAPI/Program.cs
@@ -6,13 +6,23 @@ using ReportQueryAPI.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 ConfigurationManager configuration = builder.Configuration;
+
 builder.Services.AddDbContext<ReportQueryDbContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddGrpc().AddJsonTranscoding();
 
 var app = builder.Build();
 
+ApplyMigrations(app);
+
 app.MapGrpcService<GPReportQueryService>();
 app.MapGet("/", () => "health check");
 
 app.Run();
+
+static void ApplyMigrations(WebApplication app)
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<ReportQueryDbContext>();
+    db.Database.Migrate();
+}
